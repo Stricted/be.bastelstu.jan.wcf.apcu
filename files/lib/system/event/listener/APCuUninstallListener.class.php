26ff5646522ce3ddec4dc65565bff97a0f9a03a8
Stricted <info@stricted.de>
2013-12-22 21:40:33 +0100

    add eventlistener to restore default cachesources

---
+++ files/lib/system/event/listener/APCuUninstallListener.class.php
@@ -0,0 +1,42 @@
+<?php
+namespace wcf\system\event\listener;
+use wcf\system\event\IEventListener;
+use wcf\system\WCF;
+
+/**
+ * @author      Jan Altensen (Stricted)
+ * @copyright   2012 Jan Altensen (Stricted)
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @package     de.stricted.wcf.apc
+ * @subpackage  system.event.listener
+ * @category    Community Framework
+ */
+class APCuUninstallListener implements IEventListener {
+	/**
+	 * @see \wcf\system\event\IEventListener::execute()
+	 */
+	public function execute($eventObj, $className, $eventName) {
+		if(isset($_POST['packageID']) && !empty($_POST['packageID']))
+			$packageID = intval($_POST['packageID']);
+		else
+			$packageID = 0;
+		
+		$sql = "SELECT * FROM wcf".WCF_N."_package where package = :package LIMIT 1";
+		$statement = WCF::getDB()->prepareStatement($sql);
+		$statement->execute(array(":package" => "be.bastelstu.jan.wcf.apcu"));
+		$row = $statement->fetchArray();
+		if ($packageID && $packageID == $row['packageID']) {
+			$sql = "UPDATE wcf".WCF_N."_option SET 
+	selectOptions = 'disk:wcf.acp.option.cache_source_type.disk
+memcached:wcf.acp.option.cache_source_type.memcached
+no:wcf.acp.option.cache_source_type.no',
+	enableOptions = 'disk:!cache_source_memcached_host
+memcached:cache_source_memcached_host
+no:!cache_source_memcached_host' 
+	WHERE optionName = 'cache_source_type';";
+			$statement = WCF::getDB()->prepareStatement($sql);
+			$statement->execute();
+		}
+	}
+}
+?>
